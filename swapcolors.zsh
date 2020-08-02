f343b00ed90706e51662836a945ca428b6c3555a
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-08-02 22:38:05 +0300

    Rename application specific functions

--- swapcolors.zsh
+++ swapcolors.zsh
@@ -14,27 +14,27 @@
 TEMPLATES="$HOME/.zsh/swapcolors/templates"
 
 
-configure_alacritty() {
+swapcolors_alacritty() {
   base="$TEMPLATES/alacritty/base.yml"
   color="$TEMPLATES/alacritty/$1.yml"
   cat $base $color > "$HOME/.config/alacritty/alacritty.yml"
 }
 
 
-configure_emacs() {
+swapcolors_emacs() {
   color="$TEMPLATES/emacs/$1.el"
   cp $color "$HOME/.doom.d/color.el"
 }
 
 
-configure_vim() {
+swapcolors_vim() {
   color="$TEMPLATES/vim/$1.vim"
   cp $color "$HOME/.vim/color.vim"
 }
 
 
 swapcolors() {
-  configure_alacritty $1
-  configure_emacs $1
-  configure_vim $1
+  swapcolors_alacritty $1
+  swapcolors_emacs $1
+  swapcolors_vim $1
 }
